1d74a4a44beb2a241704aabac669d5faef202dac
DragonSArmS <lagares.dimitri@gmail.com>
2023-07-19 19:59:29 -0500

    prueba9

--- src/modules/form/services/form.service.js
+++ src/modules/form/services/form.service.js
@@ -78,30 +78,30 @@ const auth = async (req, res)=>{
         email: email,
         password: inPassword
     }
-    const sql = `SELECT email, password FROM 'integrator-project'.auth WHERE email = email1@email.com'`
+    const sql = `SELECT email, password FROM 'integrator-project'.auth WHERE email = email1@email.com`
     
     connection.query(sql, async (error, rows)=>{
         console.log(rows);
-        if (error) {
-            res.json(error)
-        } else {
-           if (rows.length) {
-            const {password} = rows[0]
-            const passwordIsCorrect = await bcrypt.compare(inPassword, password)
-            const token = jwt.sign(credentials, secretKey)
-            if (passwordIsCorrect) {
-                res.json({
-                    email: rows[0].email,
-                    token: token
-                })
-            }else{
-                res.json("Wrong password")
-            }            
-           } else {
-            res.json("Wrong email")
-           }
+        // if (error) {
+        //     res.json(error)
+        // } else {
+        //    if (rows.length) {
+        //     const {password} = rows[0]
+        //     const passwordIsCorrect = await bcrypt.compare(inPassword, password)
+        //     const token = jwt.sign(credentials, secretKey)
+        //     if (passwordIsCorrect) {
+        //         res.json({
+        //             email: rows[0].email,
+        //             token: token
+        //         })
+        //     }else{
+        //         res.json("Wrong password")
+        //     }            
+        //    } else {
+        //     res.json("Wrong email")
+        //    }
             
-        }
+        // }
     })
 }
 
