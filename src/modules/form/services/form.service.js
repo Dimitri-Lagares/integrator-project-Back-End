ea13d00f3ec4c8a5b2cd2d98b49d714941160d48
DragonSArmS <lagares.dimitri@gmail.com>
2023-07-20 14:10:56 -0500

    prueba19

--- src/modules/form/services/form.service.js
+++ src/modules/form/services/form.service.js
@@ -1,8 +1,6 @@
 import connection from '../../../../config/connection.js';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import { config } from 'dotenv';
-import passport from 'passport';
 
 config()
 
@@ -13,19 +11,6 @@ const getFormData = (req, res)=>{
             res.json(error)
         } else {
             res.json(rows)
-        }
-    })
-}
-
-const sendForm = async (req, res)=>{
-    const { name, email, phone, request, comment } = req.body;
-
-    const sql = `INSERT INTO "integrator-project".form ( name, email, phone, request, comment ) VALUES ('${name}', '${email}', '${phone}', '${request}', '${comment}')`
-    connection.query(sql, (error, rows)=>{
-        if (error) {
-            res.json(error)
-        } else {
-            res.json({status: 200, message: 'Information sended succesfully'})
         }
     })
 }
@@ -71,39 +56,4 @@ const addUser = async (req, res)=>{
     })
 }
 
-
-const auth = async (req, res)=>{
-    const {email, password: inPassword} = req.body;
-    const secretKey = process.env.SECRET_AUTH
-    const credentials ={
-        email: email,
-        password: inPassword
-    }
-    const sql = `SELECT email, password FROM "integrator-project".auth WHERE email = '${email}'`
-    
-    connection.query(sql, async (error, rows)=>{
-        console.log(rows);
-        if (error) {
-            res.json(error)
-        } else {
-           if (rows.length) {
-            const {password} = rows[0]
-            const passwordIsCorrect = await bcrypt.compare(inPassword, password)
-            const token = jwt.sign(credentials, secretKey)
-            if (passwordIsCorrect) {
-                res.json({
-                    email: rows[0].email,
-                    token: token
-                })
-            }else{
-                res.json("Wrong password")
-            }            
-           } else {
-            res.json("Wrong email")
-           }
-            
-        }
-    })
-}
-
-export default { getFormData, sendForm, updateForm, deleteForm, addUser, auth }
+export default { getFormData, updateForm, deleteForm, addUser }
