c37ef0ec6e9e610b9f43904c6e13caaf41016dc5
Dimitri Lagares Gutiérrez <lagares.dimitri@gmail.com>
2023-08-02 09:35:12 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,7 @@
 import pg from 'pg'
 
 const connection = new pg.Pool({
-    user: 'user',
-    password: 'INeWtS5qOgD6kqZcK7rNeLBa0EHnhWZT',
-    host: 'ciq76q5gkuvrtobf9o70-a.oregon-postgres.render.com',
-    database: 'database_ymqr',
-    ssl:true
+    connectionString: process.env.DATABASE_URL
 })
 
 connection.connect((error)=>{
